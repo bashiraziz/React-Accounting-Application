29757c0e0c0a6356e56c51f005e69df83b9022b0
bashiraziz <bashiraziz@gmail.com>
2021-12-26 10:43:15 -0500

    Removed the menifest JSON file

--- src/config/router.js
+++ src/config/router.js
@@ -1,18 +1,17 @@
-import React, { useState, useEffect } from "react"
+//import React, { useState, useEffect } from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import ChartOfAccounts from '../Pages/ChartOfAccounts';
-import About from "../Pages/About";
+//import About from "../Pages/About";
 import Taccounts from "../Pages/Taccounts";
-import Login from "../Pages/Login";
+//import Login from "../Pages/Login";
 
 function AppRouter() {
 
     return (
         <Router>
             <Route exact path="/" component={Taccounts} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/v2" component={Taccounts} />
+            <Route exact path="/login" component={ChartOfAccounts} />
         </Router>
     )
 }
